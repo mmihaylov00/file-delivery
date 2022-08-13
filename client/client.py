17783e9d4cdbb635bca6fd85b4827bb683b8ecad
petroffD21 <pvp2004@abv.bg>
2022-08-13 17:26:12 +0300

    pomosht

--- client/client.py
+++ client/client.py
@@ -1,31 +1,37 @@
 from http import client
-from flask import Flask, render_template
-from home.views import home_view
+from flask import Flask, render_template, Blueprint
 import json
-import socket, threading, sys
+import socket
+import threading
+import sys
+from flask_socketio import SocketIO
 
-with open('config.json','r') as f:
-    data =json.load(f)
+with open('config.json', 'r') as f:
+    data = json.load(f)
 
-s = socket.socket()
-host = socket.gethostname()
-
-port = 5000
+home = Blueprint('home', __name__)
 
 @client.route("/")
 def homepage():
-    return render_template('index.html', data=data)
+    return render_template('src/index.html', data=data)
 
 
+# app = Flask(__name__)
+# app.config.from_pyfile('settingslocal.py')
+# app.register_blueprint(home)
+# socketio = SocketIO(app)
 
+ip = data["ip"]
+port = data["port"]
+extension = data["extension"]
 
-# def createApp(config_file):
-#     app = Flask(__name__) #Create application object
-#     app.config.from_pyfile(config_file) # Configure application with settings file
-#     app.register_blueprint(home_view) # Register urls so application knows what to do
-#     return app
+def createApp(config_file):
+    app = Flask(__name__) #Create application object
+    app.config.from_pyfile(config_file) # Configure application with settings file
+    app.register_blueprint(home) # Register urls so application knows what to do
+    app = SocketIO(app)    
+    return app
 
-if __name__=='__main__':
-    app = Flask(__name__) 
-    #app = createApp('settingslocal.py') # Create application with the config file
+if __name__ == '__main__':
+    app = createApp('settingslocal.py') # Create application with the config file
     app.run()
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,9 @@
 printf "Creating the file..."
 sleep 5
+cd ..
+cd client
+outputname = jq '.filename + .extension' config.json
+cd ..
+cd scripts
 cd output
-touch bashFile.txt
+touch outputname
