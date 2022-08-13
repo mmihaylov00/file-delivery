d5cc045d6bdcab5003e78d2f43b751bd1c392643
petroffD21 <pvp2004@abv.bg>
2022-08-13 18:14:23 +0300

    otivam da se samoubiq ne moga

--- client/client.py
+++ client/client.py
@@ -9,29 +9,16 @@ from flask_socketio import SocketIO
 with open('config.json', 'r') as f:
     data = json.load(f)
 
-home = Blueprint('home', __name__)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
 
-@client.route("/")
+@socketio.route('/')
 def homepage():
-    return render_template('src/index.html', data=data)
-
-
-# app = Flask(__name__)
-# app.config.from_pyfile('settingslocal.py')
-# app.register_blueprint(home)
-# socketio = SocketIO(app)
+    return 'asd'#render_template('src/index.html', data=data)
 
 ip = data["ip"]
 port = data["port"]
 extension = data["extension"]
 
-def createApp(config_file):
-    app = Flask(__name__) #Create application object
-    app.config.from_pyfile(config_file) # Configure application with settings file
-    app.register_blueprint(home) # Register urls so application knows what to do
-    app = SocketIO(app)    
-    return app
-
-if __name__ == '__main__':
-    app = createApp('settingslocal.py') # Create application with the config file
-    app.run()
+socketio.run(app)
