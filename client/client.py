bf759de6ddbb713621c0f320555ab0ba4c91e361
petroffD21 <pvp2004@abv.bg>
2022-08-13 16:56:52 +0300

    napravih neshto nz

--- client/client.py
+++ client/client.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from http import client
+from flask import Flask, render_template
 from home.views import home_view
+import json
+import socket, threading, sys
 
-def createApp(config_file):
-    app = Flask(__name__) #Create application object
-    app.config.from_pyfile(config_file) # Configure application with settings file
-    app.register_blueprint(home_view) # Register urls so application knows what to do
-    return app
+with open('config.json','r') as f:
+    data =json.load(f)
+
+s = socket.socket()
+host = socket.gethostname()
+
+port = 5000
+
+@client.route("/")
+def homepage():
+    return render_template('index.html', data=data)
+
+
+
+
+# def createApp(config_file):
+#     app = Flask(__name__) #Create application object
+#     app.config.from_pyfile(config_file) # Configure application with settings file
+#     app.register_blueprint(home_view) # Register urls so application knows what to do
+#     return app
 
 if __name__=='__main__':
-    app = createApp('settingslocal.py') # Create application with the config file
+    app = Flask(__name__) 
+    #app = createApp('settingslocal.py') # Create application with the config file
     app.run()
